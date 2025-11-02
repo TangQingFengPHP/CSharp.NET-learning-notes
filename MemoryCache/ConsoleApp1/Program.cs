58ea8fba57bbd16b5344fe3d3c9798f7e82702c0
panfeng <TangQingFengPHP@protonmail.com>
2025-11-02 14:51:36 +0800

    MemoryCache 用法示例

---
+++ MemoryCache/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Caching;
+
+var cache = MemoryCache.Default;
+CacheItemPolicy policy = new CacheItemPolicy
+{
+    AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(10), // 设置绝对过期时间
+    SlidingExpiration = TimeSpan.Zero,           // 设置滑动过期时间
+    Priority = CacheItemPriority.Default,                  // 设置优先级
+    RemovedCallback = args => { Console.WriteLine("缓存项已移除"); } // 过期时执行回调
+};
+
+cache.Add("key1", "value1", policy);
+
+Console.WriteLine(cache.Get("key1")); // 输出：value1
